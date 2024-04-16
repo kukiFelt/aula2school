7ed94530799c22010f5c32fabca5636488498fbf
kukiFelt <108556456+kukiFelt@users.noreply.github.com>
2024-04-16 23:27:42 +0000

    fix: Titulo

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function home() {
   return(
-    <h1 className="text-center">ESCOLA TECNOLOGICA</h1>
+    <h1 className="text-center">ESCOLA TECNOLOGICA AUTUALIZADO</h1>
   )
 }
