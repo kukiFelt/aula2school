49154e39e76c3b54bb4c7d085cb2cb27f00d3aa7
kukiFelt <108556456+kukiFelt@users.noreply.github.com>
2024-04-17 00:57:06 +0000

    feat: courses e footer

---
+++ app/components/Courses.tsx
@@ -0,0 +1,67 @@
+export default function Courses(){
+    return(
+        <><div className="font-bold">
+            <h3>CONHEÇA NOSSOS <span className="text-[#00e77f]">CURSOS</span></h3>
+        </div><div className="mt-8 grid gap-4 md:grid-cols-2 items-center lg:grid-cols-3">
+                <div className="bg-[#4d4d4d] rounded-md lg:rounded-lg">
+                    <a href="https://www.blackbox.ai/">
+                        <img src="/img/html.svg" alt="" />
+                        <div>
+                            <h4>CURSO DE HTML</h4>
+                            <p>O curso de HTML é oferecido....</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div className="bg-[#4d4d4d] rounded-md lg:rounded-lg">
+                    <a href="https://www.blackbox.ai/">
+                        <img src="/img/css.svg" alt="" />
+                        <div>
+                            <h4>CURSO DE CSS</h4>
+                            <p>O curso de css é oferecido....</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div className="bg-[#4d4d4d] rounded-md lg:rounded-lg">
+                    <a href="https://www.blackbox.ai/">
+                        <img src="/img/js.svg" alt="" />
+                        <div>
+                            <h4>CURSO DE JAVA SCRIPT</h4>
+                            <p>O curso de js é oferecido....</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div className="bg-[#4d4d4d] rounded-md lg:rounded-lg">
+                    <a href="https://www.blackbox.ai/">
+                        <img src="/img/games.svg" alt="" />
+                        <div>
+                            <h4>CURSO DE GAMES</h4>
+                            <p>O curso de games é oferecido....</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div className="bg-[#4d4d4d] rounded-md lg:rounded-lg">
+                    <a href="https://www.blackbox.ai/">
+                        <img src="/img/design.svg" alt="" />
+                        <div>
+                            <h4>CURSO DE DESING</h4>
+                            <p>O curso de desing é oferecido....</p>
+                        </div>
+                    </a>
+                </div>
+
+                <div className="bg-[#4d4d4d] rounded-md lg:rounded-lg">
+                    <a href="https://www.blackbox.ai/">
+                        <img src="/img/robot.svg" alt="" />
+                        <div>
+                            <h4>CURSO DE ROBOT</h4>
+                            <p>O curso de robot é oferecido....</p>
+                        </div>
+                    </a>
+                </div>
+            </div></>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
+import Courses from "./components/Courses";
+import Footer from "./components/Footer";
 
 export default function home() {
   return(
     <div>
     <Header/>
     <Banner/>
+    <Courses/>
+    <Footer/>
     </div>
   )
 }
