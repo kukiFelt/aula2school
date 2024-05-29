8481b30743f574cc3da32d3874bbfb1ed4387da0
kukiFelt <108556456+kukiFelt@users.noreply.github.com>
2024-05-29 00:43:33 +0000

    feat: cadastro e listagem de estudantes

--- app/admin/student/ListStudent.tsx
+++ app/admin/student/ListStudent.tsx
@@ -1,88 +1,106 @@
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableFooter,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/table"
-  
-  const invoices = [
-    {
-      invoice: "INV001",
-      paymentStatus: "Paid",
-      totalAmount: "$250.00",
-      paymentMethod: "Credit Card",
-    },
-    {
-      invoice: "INV002",
-      paymentStatus: "Pending",
-      totalAmount: "$150.00",
-      paymentMethod: "PayPal",
-    },
-    {
-      invoice: "INV003",
-      paymentStatus: "Unpaid",
-      totalAmount: "$350.00",
-      paymentMethod: "Bank Transfer",
-    },
-    {
-      invoice: "INV004",
-      paymentStatus: "Paid",
-      totalAmount: "$450.00",
-      paymentMethod: "Credit Card",
-    },
-    {
-      invoice: "INV005",
-      paymentStatus: "Paid",
-      totalAmount: "$550.00",
-      paymentMethod: "PayPal",
-    },
-    {
-      invoice: "INV006",
-      paymentStatus: "Pending",
-      totalAmount: "$200.00",
-      paymentMethod: "Bank Transfer",
-    },
-    {
-      invoice: "INV007",
-      paymentStatus: "Unpaid",
-      totalAmount: "$300.00",
-      paymentMethod: "Credit Card",
-    },
-  ]
-  
-  export default function ListStudent() {
-    return (
-      <Table>
-        <TableCaption>A list of your recent invoices.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Invoice</TableHead>
-            <TableHead>RA</TableHead>
-            <TableHead>Nome</TableHead>
-            <TableHead className="text-right">Email</TableHead>
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { revalidatePath } from "next/cache"
+
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+]
+
+interface IStudent {
+  id:number,
+  name:string,
+  email:string
+}
+
+export default async function ListStudent() {
+  const stundents = await list()
+  async function list() {
+    revalidatePath("/admin/student")
+    const response = await fetch("https://server20241.vercel.app/students")
+    return response.json();
+  }
+
+  // const stundents = await list()
+  // async function list() {
+  //   const response = await fetch("https://server20241.vercel.app/students")
+  //   return response.json();
+  // } para ler do banco os dados e ir para a tabela
+
+  return (
+    <Table>
+      <TableCaption>Lista de Estudantes.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">ID</TableHead>
+          <TableHead>Nome</TableHead>
+          <TableHead>Email</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {stundents.map((item:IStudent) => (
+          <TableRow key={item.id}>
+            <TableCell className="font-medium">{item.id}</TableCell>
+            <TableCell>{item.name}</TableCell>
+            <TableCell>{item.email}</TableCell>
           </TableRow>
-        </TableHeader>
-        <TableBody>
-          {invoices.map((invoice) => (
-            <TableRow key={invoice.invoice}>
-              <TableCell className="font-medium">{invoice.invoice}</TableCell>
-              <TableCell>{invoice.paymentStatus}</TableCell>
-              <TableCell>{invoice.paymentMethod}</TableCell>
-              <TableCell className="text-right">{invoice.totalAmount}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-        {/* <TableFooter>
+        ))}
+      </TableBody>
+      {/* <TableFooter>
           <TableRow>
             <TableCell colSpan={3}>Total</TableCell>
             <TableCell className="text-right">$2,500.00</TableCell>
           </TableRow>
         </TableFooter>  trabalha com total*/}
-      </Table>
-    )
-  }
-  
+    </Table>
+  )
+}
+// className="text-right"  Deixa o texto a direita
--- app/admin/student/page.tsx
+++ app/admin/student/page.tsx
@@ -1,15 +1,17 @@
-import  ListStudent  from "./ListStudent";
-import NewStudent from "./New";
+import { Button } from "@/components/ui/button";
+import ListStudent from "./ListStudent";
 
 export default function Student() {
     return (
         <div className="w-full flex flex-col justify-center mt-6">
             <div className="flex justify-center mb-6">
-                <NewStudent />
+                <a href="/admin/student/new">
+                    <Button>Cadastrar Estudante</Button>
+                </a>
             </div>
 
             <ListStudent />
         </div>
     )
 }
-// w-full aumenta o tamnho da DIV
+// w-full aumenta o tamanho da DIV
